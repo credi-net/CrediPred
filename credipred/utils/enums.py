57fe7f418bb922a1ae3e5791d545f523e5d0aeec
sebastian9991 <sebastian.sabry1@gmail.com>
2026-01-21 17:57:59 -0500

    dedicated enums file under utilites

---
+++ credipred/utils/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Scoring(str, Enum):
+    mse = 'MSE'
+    r2 = 'R2'
+    mae = 'MAE'
+
+
+class Metric(str, Enum):
+    acc = 'Accuracy'
+    loss = 'Loss'
+
+
+class Label(str, Enum):
+    pc1 = 'PC1'
+    mbfc = 'MBFC-BIAS'
