8e53d5d4c945cca7a53397eb1c7c93417d24c7b8
sebastian9991 <sebastian.sabry1@gmail.com>
2026-03-13 15:55:45 -0400

    rename log file

--- scripts/get_all_embeddings.py
+++ scripts/get_all_embeddings.py
@@ -143,7 +143,7 @@ def main() -> None:
     args = parser.parse_args()
     config_file_path = root / args.config_file
     meta_args, experiment_args = parse_args(config_file_path)
-    setup_logging(meta_args.log_file_path)
+    setup_logging(cast(str, meta_args.log_file_path) + 'GET_EMBEDDINGS.log')
     seed_everything(meta_args.global_seed)
 
     encoder_classes: Dict[str, Encoder] = {
