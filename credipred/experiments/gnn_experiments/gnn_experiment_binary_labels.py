b4780c548c7c0f2aecd63bc133d9d579388f6648
sebastian9991 <sebastian.sabry1@gmail.com>
2026-04-07 23:52:39 -0400

    updates

--- credipred/experiments/gnn_experiments/gnn_experiment_binary_labels.py
+++ credipred/experiments/gnn_experiments/gnn_experiment_binary_labels.py
@@ -12,8 +12,6 @@ from credipred.gnn.model import Model
 from credipred.utils.args import DataArguments, ModelArguments
 from credipred.utils.enums import Metric, TrainingMethods
 from credipred.utils.logger import Logger
-from credipred.utils.plot import Scoring, plot_avg_loss
-from credipred.utils.save import save_loss_results
 
 
 def train_(
@@ -286,11 +284,3 @@ def run_binary_class_gnn_baseline(
     logging.info(f'Model: {model_arguments} weights saved to: {best_model_path}')
     logging.info('*** Statistics ***')
     logging.info(logger.get_statistics(metric=Metric.acc, higher_is_better=True))
-    logging.info('Constructing plots')
-    plot_avg_loss(
-        loss_tuple_run_mse, model_arguments.model, Scoring.acc, 'loss_plot.png'
-    )
-    logging.info('Saving pkl of results')
-    save_loss_results(
-        loss_tuple_run_mse, model_arguments.model, 'binary_classification'
-    )
